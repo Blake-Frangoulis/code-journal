ef488ae970019b7ff668c035644893a00f6406b3
Blake Frangoulis <blakef44@gmail.com>
2023-05-31 21:52:32 +0000

    added beforeunload event listener to serialize data as json and store to local storage. added conditional statement to parse previous stringified data and add to data variable

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  const stringifiedData = JSON.stringify(data);
+  localStorage.setItem('journal-local-storage', stringifiedData);
+});
+
+const previousStringifiedData = localStorage.getItem('journal-local-storage');
+if (previousStringifiedData !== null) {
+  data = JSON.parse(previousStringifiedData);
+}
