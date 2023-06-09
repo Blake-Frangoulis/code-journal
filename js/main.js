10fc9f97ac0000fc59abace4c14a96a13b892a15
Blake Frangoulis <blakef44@gmail.com>
2023-06-09 19:13:48 +0000

    added to 'DOMContentLoaded' event listener to make it: use viewswap function to show form, find the entry object whose id matches the pencil icon clicked and assn that object the data.editing property, pre-populate entry form with clicked entry's values, and to update the title of the entry-form view to 'Edit Entry'

--- js/main.js
+++ js/main.js
@@ -70,6 +70,17 @@ document.addEventListener('DOMContentLoaded', function (event) {
   for (let i = 0; i < data.entries.length; i++) {
     const output = renderEntry(data.entries[i]);
     $ul.appendChild(output);
+    const $pencilIcon = output.querySelector('.fa-pencil');
+    $pencilIcon.addEventListener('click', () => {
+      viewSwap('entry-form');
+      const selectedEntry = data.entries[i];
+      data.editing = selectedEntry;
+      $form.elements.title.value = selectedEntry.title;
+      $form.elements['photo-url'].value = selectedEntry.photoUrl;
+      $form.elements.notes.value = selectedEntry.notes;
+      const $entryFormh1 = document.querySelector('.new-entry h1');
+      $entryFormh1.textContent = 'Edit Entry';
+    });
   }
   viewSwap(data.view);
   toggleNoEntries();
